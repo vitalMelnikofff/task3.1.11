367390d87b96667582e0cc6781379564c43802d6
vitalMelnikofff <melnickov.vitalick2013@yandex.ru>
2023-08-08 00:46:50 +0300

    Task 3/1/12

--- SpringBoot/src/test/java/ru/melnikov/SpringBoot/ApplicationTests.java
+++ SpringBoot/src/test/java/ru/melnikov/SpringBoot/ApplicationTests.java
@@ -1,13 +1,16 @@
 package ru.melnikov.SpringBoot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
 
-	@Test
-	void contextLoads() {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
 	}
 
 }
+
+
